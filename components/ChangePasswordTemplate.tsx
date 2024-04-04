987d2e2dea3504a7f22ddff0b424960dfe1b64fb
Roshan Roy <roshanroy2911@gmail.com>
2024-04-04 16:27:32 +0530

    initial commit

--- components/ChangePasswordTemplate.tsx
+++ components/ChangePasswordTemplate.tsx
@@ -12,6 +12,7 @@ export const ChangePasswordTemplate: React.FC<Readonly<changePasswordProps>> = (
     <div>
         <h1>Change Password for {email}</h1>
         <p>To change your password click on this link :</p>
+        <h2>{process.env.NEXTAUTH_URL}</h2>
         <a href={`${process.env.NEXTAUTH_URL}/auth/forgot-password?token=${changePasswordToken}`}>
             Click here to change your password
         </a>
