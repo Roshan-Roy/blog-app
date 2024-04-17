47bc2e3df7c6f40f682c6161450cda96fe87396e
Roshan Roy <roshanroy2911@gmail.com>
2024-04-17 13:38:23 +0530

    image adding

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const ImageCard = ({ url }: { url: string | null }) => {
     return (
         <div>
-            <Image src={url as string} alt="Image" width={250} height={250} unoptimized/>
+            <Image src={url as string} alt="Image" width={250} height={250}/>
         </div>
     )
 }
