5726b1e0842177c78d1ed81485359299d729f96a
Roshan Roy <roshanroy2911@gmail.com>
2024-04-18 16:20:21 +0530

    image adding

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -1,15 +1,8 @@
 import ImageCard from "@/components/ImageCard"
 import { prisma } from "@/lib/db"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
 
 const Images = async () => {
-    const session = await getServerSession(authOptions)
-    const imageList = await prisma.image.findMany({
-        where: {
-            userId: session?.user.id
-        }
-    })
+    const imageList = await prisma.image.findMany()
     return (
         <div>
             {imageList.map((e, i) => <ImageCard key={i} url={e.src} />)}
