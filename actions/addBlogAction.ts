97a591af17ca6533c3f61546c05e38e0a4df7b0e
Roshan Roy <roshanroy2911@gmail.com>
2024-04-24 23:03:33 +0530

    add blog on profile

--- actions/addBlogAction.ts
+++ actions/addBlogAction.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { prisma } from "@/lib/db"
+import { revalidatePath } from "next/cache";
 
 export const addBlogAction = async ({ title, content, category, userId }: {
     title: string;
@@ -8,7 +9,6 @@ export const addBlogAction = async ({ title, content, category, userId }: {
     category: string;
     userId: string | undefined;
 }) => {
-    console.log("loading")
     await prisma.blog.create({
         data: {
             title,
@@ -17,5 +17,5 @@ export const addBlogAction = async ({ title, content, category, userId }: {
             userId
         }
     })
-    console.log("Blog created")
+    revalidatePath("/")
 }
