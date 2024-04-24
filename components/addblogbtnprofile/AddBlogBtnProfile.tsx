6b730953642b3806255eb3a4aa2dbeaaafd65e2d
Roshan Roy <roshanroy2911@gmail.com>
2024-04-24 22:57:59 +0530

    add blog on profile

---
+++ components/addblogbtnprofile/AddBlogBtnProfile.tsx
@@ -0,0 +1,9 @@
+import styles from "./addblogbtnprofile.module.css"
+
+const AddBlogBtnProfile = () => {
+    return (
+        <button className={styles.btn}>+</button>
+    )
+}
+
+export default AddBlogBtnProfile
