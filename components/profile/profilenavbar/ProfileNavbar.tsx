25f2fc528d281945b831674499dbe004cda81983
Roshan Roy <roshanroy2911@gmail.com>
2024-04-24 22:51:09 +0530

    add blog on profile

--- components/profile/profilenavbar/ProfileNavbar.tsx
+++ components/profile/profilenavbar/ProfileNavbar.tsx
@@ -3,7 +3,8 @@
 import styles from "./profilenavbar.module.css"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-
+import AddBlog from "@/components/addblog/AddBlog"
+import AddBlogBtnProfile from "@/components/addblogbtnprofile/AddBlogBtnProfile"
 
 
 const ProfileNavbar = () => {
@@ -11,7 +12,9 @@ const ProfileNavbar = () => {
     return (
         <div className={styles.container}>
             <Link className={pathName === "/profile/blogs" ? styles.active : undefined} href="/profile/blogs">Blogs</Link>
-            
+            <AddBlog>
+                <AddBlogBtnProfile/> G
+            </AddBlog>
             <Link className={pathName === "/profile/saved" ? styles.active : undefined} href="/profile/saved">Saved</Link>
         </div>
     )
